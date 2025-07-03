a80a7e3ec10756545c50627e9cff1daf46f952cf
NiallMahon <niallcmahon@gmail.com>
2025-07-03 12:44:04 +0100

    Update AddRow.js
    Now includes a fetch to the backend for updating MySQL.

--- js/AddRow.js
+++ js/AddRow.js
@@ -1,18 +1,25 @@
-  var count = 1;
-        function addrow() {
-            var table = document.getElementById("tableid");
-            var rowCount = table.rows.length;
-            var colCount = table.rows[0].cells.length;
-              var newRow = table.insertRow(rowCount);
-            for (var i = 0; i<colCount; i++) {
-               var cell = newRow.insertCell(i);
-                if (i === 0) {
-                    cell.innerHTML = count;
-                    cell.style.height="80px";
-                    count++;
+  let count = 1;
+  let counter = 1;
+            function addrow() {
+                var table = document.getElementById("tableid");
+                var rowCount = table.rows.length;
+                var colCount = table.rows[0].cells.length;
+                var newRow = table.insertRow(rowCount);
+                for (var i = 0; i<colCount; i++) {
+                var cell = newRow.insertCell(i);
+                    if (i === 0) {
+                        cell.innerHTML = count;
+                        cell.style.height="80px";
+                        count++;
+                    }
+                    else {
+                        cell.innerHTML ='<button class="clic" style="height: 80px;"></button>';
+                    }
                 }
-                else {
-                    cell.innerHTML ='<button class="clic" style="height: 90px;"></button>';
-                }
+
+                fetch(`http://localhost:3000/add-row/${colCount}`, {
+                    method: "POST",
+                })
+                .then((res)=>res.text())
+                .then((text)=>console.log(text))
             }
-        }
