4c8cb4b877e9fbcdac591c8581e249f7b2901c11
NiallMahon <niallcmahon@gmail.com>
2025-07-03 12:45:03 +0100

    Update AddColumn.js
    Now includes fetch to backend to update MySQL

--- js/AddColumn.js
+++ js/AddColumn.js
@@ -1,16 +1,25 @@
+  function addcol(){
+                var table = document.getElementById("tableid");
+                var rowCount = table.rows.length;
+                var colCount = table.rows[0].cells.length;
+                
+                for (var i = 0; i < rowCount; i++){
+                    var cell = table.rows[i].insertCell(table.rows[i].cells.length);
+                    if (i ===0){
+                        cell.innerHTML ='<input type="text" onkeyup="goalName()" class="goal" placeholder="GOAL">';
+                        
+                    
+                    }
+                    else {
+                        cell.innerHTML = '<button  class="clic"  style="height: 80px;"></button>';
+                    }
+                }
+                //ADD COL
+                fetch(`http://localhost:3000/add-col/${counter}`, {
+                    method: "POST",
+                })
+                .then((res) =>res.text())
+                .then((text)=>console.log(text))
 
-        function addcol(){
-            var table = document.getElementById("tableid");
-            var rowCount = table.rows.length;
-            var colCount = table.rows[0].cells.length;
-            
-            for (var i = 0; i < rowCount; i++){
-                var cell = table.rows[i].insertCell(table.rows[i].cells.length);
-                if (i ===0){
-                    cell.innerHTML ='<input type="text" placeholder="goal">';
-                }
-                else {
-                    cell.innerHTML = '<button  class="clic"  style="height: 80px;"></button>';
-                }
+                counter ++;
             }
-        }
