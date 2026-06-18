9d3f0e2375c1281f3b1cd51a35c00359854f5e98
NiallMahon <niallcmahon@gmail.com>
2026-06-18 14:33:05 +0100

    node setup 2

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.listen(3000, ()=>{
-    console.log("running on port 3000")
+app.listen(3001, ()=>{
+    console.log("running on port 3001")
 })
+
